d0ff39cd76619147c2b66b4597616c23df79769b
Stefan Marinov <stefan_marinov@rogers.com>
2023-02-23 18:45:38 -0500

    copmleted category post / route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,8 +35,27 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const { categoryName } = req.body;
+  if (!categoryName) {
+    res.status(500).json({message: "Improperly formatted request"});
+    return;
+  }
+
+  try {
+    const newCategory = await Category.create({category_name: categoryName});
+    
+    if (!newCategory) {
+      res.status(500).json({message: "Could not create the new category! Please try again later!"});
+      return;  
+    }
+    res.status(200).json(newCategory);
+
+  } catch (err) {
+    res.status(500).json({message: "Could not create the new category! Please try again later!"});
+    return;
+  }
 });
 
 router.put('/:id', (req, res) => {
