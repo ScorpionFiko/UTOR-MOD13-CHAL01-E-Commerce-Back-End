7d831d94211c4419a770e143a4bcf2a8bcd6d169
Stefan Marinov <stefan_marinov@rogers.com>
2023-02-23 18:28:30 -0500

    copmleted category get /:id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,7 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  // find all categories and associated Products
   try {
     const categoryData = await Category.findAll({
         include: [{ model: Product }]
@@ -20,9 +19,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` and its associated PRoducts
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+        include: [{ model: Product }]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: `No category with id: ${req.params.id} found!` });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
