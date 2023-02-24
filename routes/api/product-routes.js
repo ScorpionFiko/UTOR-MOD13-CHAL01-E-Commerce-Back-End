d480d163c11bbc73c58f25c0d4a2fbd0dc70724c
Stefan Marinov <stefan_marinov@rogers.com>
2023-02-23 20:20:07 -0500

    copmleted product routes and model updates

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,30 +1,51 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  // find all products with associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: `product_tags` }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No data found!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  // find a product by its id with associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id,{
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: `product_tags` }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: `No data found for product with id: ${req.params.id}!` });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+
+  }
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+  const { product_name, category_id, price, stock, tagIds } = req.body;
+  if (!product_name || !category_id || !price || !stock || !tagIds ) {
+    return res.status(500).json({message: "Improperly formatted request!"});
+  }
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -49,6 +70,11 @@ router.post('/', (req, res) => {
 
 // update product
 router.put('/:id', (req, res) => {
+  const { product_name, category_id, price, stock, tagIds } = req.body;
+  if (!product_name || !category_id || !price || !stock || !tagIds ) {
+    return res.status(500).json({message: "Improperly formatted request!"});
+  }
+
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,8 +115,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deletedProduct) {
+      res.status(404).json({message: `No product with id: ${req.params.id} found!`});
+      return;
+    }
+    res.status(200).json(deletedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
